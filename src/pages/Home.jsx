ae80b338171d7e625d73dd20482a8e4c50b480ef
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-11-09 17:49:12 -0300

    Adicionado funcao handlequerybutton, onclick do botao, setado estados

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import { getProductFromQuery } from '../services/api';
 
 class Home extends Component {
   state = {
+    inputQuery: '',
+  };
 
+  handleQueryButton = async (query) => {
+    const queryResult = await getProductFromQuery(query);
+    this.setState({
+      queryResult,
+    });
   };
 
   render() {
+    const { inputQuery, queryResult } = this.state;
     return (
       <div>
         <input data-testid="query-input" type="text" />
-        <button data-testid="query-button" type="submit">Buscar</button>
+        <button
+          data-testid="query-button"
+          type="submit"
+          onClick={ () => this.handleQueryButton(inputQuery) }
+        >
+          Buscar
+        </button>
       </div>
     );
   }
