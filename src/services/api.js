62036f6c33650957dc3b70c0b4fb2029fe771b53
LeandreXFC <leandro.sousa23tec@gmail.com>
2022-11-09 16:32:29 -0300

    Segunda função do req 1

--- src/services/api.js
+++ src/services/api.js
@@ -2,12 +2,15 @@
 
 export async function getCategories() {
   const response = await fetch(' https://api.mercadolibre.com/sites/MLB/categories');
-  const result = response.json();
+  const result = await response.json();
   return result;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const response = await fetch(url);
+  const result = await response.json();
+  return result;
 }
 
 export async function getProductById() {
