e422405ae6e0bcd17bf181f38aa77057fb82d9bc
Múria de Oliveira Abrantes <muria.abrantees@gmail.com>
2022-11-09 18:15:05 -0300

    Requisito 4: ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Categories from '../components/Categories';
 
 class Home extends Component {
   render() {
@@ -7,6 +8,7 @@ class Home extends Component {
         <span data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </span>
+        <Categories />
       </div>
     );
   }
