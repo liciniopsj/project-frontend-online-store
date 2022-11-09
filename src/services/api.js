151ddaa0e888e5b219f3fd473aeef87667d921b9
LeandreXFC <leandro.sousa23tec@gmail.com>
2022-11-09 16:37:12 -0300

    terceira função do req 3

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,12 @@
 // função que pega as categorias da api.
 
 export async function getCategories() {
-  const response = await fetch(' https://api.mercadolibre.com/sites/MLB/categories');
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
   const result = await response.json();
   return result;
 }
 
+// função que busca itens dentro de uma categoria específica.
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   const response = await fetch(url);
@@ -13,7 +14,9 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   return result;
 }
 
-export async function getProductById() {
-  // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
-  // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
+export async function getProductById(productId) {
+  const url = `https://api.mercadolibre.com/items/${productId}`;
+  const response = await fetch(url);
+  const result = await response.json();
+  return result;
 }
