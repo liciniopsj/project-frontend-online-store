65b3b56c346a246504e1f570c1c3d5dc13fa70e3
thiagohcc <dzno23@gmail.com>
2022-11-14 21:37:13 -0300

    inicia formulário e lógica de estrelas para hating

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,10 +3,22 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { getProductById } from '../services/api';
 import { handleButtonAddCart } from '../services/ShoppingCartButtons';
+import './Product.css';
+
+const starGrey = { color: 'lightgrey' };
+const starYellow = { color: 'yellow' };
 
 class Product extends Component {
   state = {
     product: {},
+    email: '',
+    hating: 0,
+    message: '',
+    star1: starGrey,
+    star2: starGrey,
+    star3: starGrey,
+    star4: starGrey,
+    star5: starGrey,
   };
 
   async componentDidMount() {
@@ -19,8 +31,45 @@ class Product extends Component {
     });
   }
 
+  onInputChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  colorStar = ({ target }) => {
+    const { id } = target;
+    for (let i = 1; i <= id; i += 1) {
+      this.setState({
+        [`star${i}`]: starYellow,
+      });
+    }
+  };
+
+  uncolorStar = ({ target }) => {
+    const { id } = target;
+    for (let i = 1; i <= id; i += 1) {
+      this.setState({
+        [`star${i}`]: starGrey,
+      });
+    }
+  };
+
+  setHatingStars = (event) => {
+    event.preventDefault();
+    const { id } = event.target;
+    for (let i = 1; i <= id; i += 1) {
+      this.setState({
+        [`star${i}`]: starYellow,
+        hating: i,
+      });
+    }
+  };
+
   render() {
-    const { product } = this.state;
+    const { product, email, message, star1, star2, star3, star4, star5 } = this.state;
     return (
       <div>
         <h2 data-testid="product-detail-name">{ product.title }</h2>
@@ -55,6 +104,106 @@ class Product extends Component {
         >
           Adicionar ao carrinho
         </button>
+        <section>
+          <h3>Avaliações:</h3>
+          <form>
+            <input
+              type="text"
+              name="email"
+              placeholder="email"
+              data-testid="product-detail-email"
+              onChange={ this.onInputChange }
+              value={ email }
+            />
+
+            <div>
+              <button
+                type="submit"
+                id="1"
+                className="star star-1"
+                data-testid="1-rating"
+                onMouseOver={ this.colorStar }
+                onMouseOut={ this.uncolorStar }
+                onClick={ this.setHatingStars }
+                onBlur={ () => {} }
+                onFocus={ () => {} }
+                style={ star1 }
+              >
+                ★
+              </button>
+              <button
+                type="submit"
+                id="2"
+                className="star star-2"
+                data-testid="2-rating"
+                onMouseOver={ this.colorStar }
+                onMouseOut={ this.uncolorStar }
+                onBlur={ () => {} }
+                onFocus={ () => {} }
+                style={ star2 }
+              >
+                ★
+              </button>
+              <button
+                type="submit"
+                id="3"
+                className="star star-3"
+                data-testid="3-rating"
+                onMouseOver={ this.colorStar }
+                onMouseOut={ this.uncolorStar }
+                onBlur={ () => {} }
+                onFocus={ () => {} }
+                style={ star3 }
+              >
+                ★
+              </button>
+              <button
+                type="submit"
+                id="4"
+                className="star star-4"
+                data-testid="4-rating"
+                onMouseOver={ this.colorStar }
+                onMouseOut={ this.uncolorStar }
+                onBlur={ () => {} }
+                onFocus={ () => {} }
+                style={ star4 }
+              >
+                ★
+              </button>
+              <button
+                type="submit"
+                id="5"
+                className="star star-5"
+                data-testid="5-rating"
+                onMouseOver={ this.colorStar }
+                onMouseOut={ this.uncolorStar }
+                onBlur={ () => {} }
+                onFocus={ () => {} }
+                style={ star5 }
+              >
+                ★
+              </button>
+            </div>
+
+            <input
+              type="textarea"
+              name="message"
+              placeholder="Mensagem (opcional)"
+              data-testid="product-detail-evaluation"
+              onChange={ this.onInputChange }
+              value={ message }
+            />
+
+            <button
+              type="submit"
+              data-testid="submit-review-btn"
+            >
+              Avaliar produto!
+            </button>
+
+          </form>
+
+        </section>
       </div>
     );
   }
